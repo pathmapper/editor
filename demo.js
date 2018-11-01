fdfecd114199fa64d51e7666322bc36ac5f40466
pathmapper <pathmapper@posteo.de>
2018-11-01 11:52:55 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,6 +3,6 @@
 const bot = require("circle-github-bot").create();
  
 bot.comment(`
-//<h3>${bot.env.commitMessage}</h3>
-Test build: <strong>${bot.artifactLink('../../artifacts/build/index.html', 'demo')}</strong>
+${bot.env.commitMessage}
+<strong>${bot.artifactLink('../../artifacts/build/index.html', 'Test build')}</strong>
 `);
