edd032b0cab0737faebdef1be7b4b34a47e05393
pathmapper <pathmapper@posteo.de>
2018-11-01 17:06:42 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,11 @@
-#!/usr/bin/env node
- 
+if (!process.env.GH_AUTH_TOKEN) {
+    console.error("Missing GH_AUTH_TOKEN env var, refusing to run.");
+    process.exit();
+}
+
 const bot = require("circle-github-bot").create();
- 
+
 bot.comment(`
-<h3>${bot.env.commitMessage}</h3>
-Demo: <strong>${bot.artifactLink('/artifacts/build/index.html', 'demo')}</strong>
+<h4>${bot.env.commitMessage}</h4>
+Preview: <strong>${bot.artifactLink('../../artifacts/build/index.html', 'Maputnik test build')}</strong>
 `);
