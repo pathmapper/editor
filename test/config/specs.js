9cab04ca35d0ae921c039d419bb80e5f280c0b58
pathmapper <pathmapper@posteo.de>
2022-02-12 11:53:07 +0100

    editor

--- test/config/specs.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-var testNetwork = process.env.TEST_NETWORK || "localhost";
-
-config.port = 9001;
-config.baseUrl = "http://"+testNetwork+":"+config.port;
-
-module.exports = config;
